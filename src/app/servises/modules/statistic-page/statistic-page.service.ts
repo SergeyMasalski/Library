ef85bd89f91b7ded3064b8948738a90ee1502be8
Sergey Masalski <masalskiysergey@gmail.com>
2023-07-16 16:34:46 +0300

    feat: add statistic

--- src/app/servises/modules/statistic-page/statistic-page.service.ts
+++ src/app/servises/modules/statistic-page/statistic-page.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Statistic } from 'src/app/entities/enums/statistic.enum';
+import { CommenApplicationNamespace } from 'src/app/entities/interfaces/app.interfaces';
+import { CardPageService } from '../card-page/card-page.service';
+import { BookPageService } from '../book-page/book-page.service';
+import { VisitorsPageService } from '../visitors-page/visitors-page.service';
 
 @Injectable()
 export class StatisticPageService {
-  private statisticHeader: string = Statistic.MostActiveUsers;
+  private statisticHeader: string = Statistic.MostPopularBooks;
+
+  constructor(
+    private cardService: CardPageService,
+    private booksService: BookPageService,
+    private visitorsService: VisitorsPageService
+  ) {}
 
   get getStatisticHeader() {
     return this.statisticHeader;
@@ -11,5 +21,49 @@ export class StatisticPageService {
 
   set setStatisticHeader(value: string) {
     this.statisticHeader = value;
+  }
+
+  get getPopularBooks(): (string | undefined)[] {
+    const statisticArray: CommenApplicationNamespace.PopularBooks[] = [];
+    const arrayBooksIDs = this.cardService.getCards.map((item) => item.bookId);
+    for (let i = 1; i <= Math.max(...arrayBooksIDs); i++) {
+      const filterArrayBooksIDs = arrayBooksIDs.filter(
+        (bookId) => bookId === i
+      );
+      statisticArray.push({ idBook: i, quantity: filterArrayBooksIDs.length });
+    }
+
+    if (statisticArray) {
+    }
+    return statisticArray
+      .sort((a, b) => b.quantity - a.quantity)
+      .map((item) => this.booksService.getBook(`${item.idBook}`)?.name)
+      .slice(0, 5);
+  }
+
+  get getActiveUsers(): (string | undefined)[] {
+    const statisticArray: CommenApplicationNamespace.ActiveUsers[] = [];
+    const arrayVisitorsIDs = this.cardService.getCards.map(
+      (item) => item.visitorId
+    );
+    for (let i = 1; i <= Math.max(...arrayVisitorsIDs); i++) {
+      const filterArrayVisitorsIDs = arrayVisitorsIDs.filter(
+        (bookId) => bookId === i
+      );
+      statisticArray.push({
+        idUser: i,
+        quantity: filterArrayVisitorsIDs.length,
+      });
+    }
+
+    return statisticArray
+      .sort((a, b) => b.quantity - a.quantity)
+      .map(
+        (item) =>
+          this.visitorsService.getVisitor(`${item.idUser}`)?.visitorFullName
+      )
+      .slice(0, 5);
+
+    return [];
   }
 }
--- src/app/statistic-page/statistic-table/statistic-table.component.ts
+++ src/app/statistic-page/statistic-table/statistic-table.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Statistic } from 'src/app/entities/enums/statistic.enum';
+import { StatisticPageService } from 'src/app/servises/modules/statistic-page/statistic-page.service';
 
 @Component({
   selector: 'statistic-table',
   templateUrl: './statistic-table.component.html',
   styleUrls: ['./statistic-table.component.scss'],
 })
-export class StatisticTableComponent {}
+export class StatisticTableComponent {
+  constructor(private statisticService: StatisticPageService) {}
+
+  get getList(): (string | undefined)[] {
+    if (
+      this.statisticService.getStatisticHeader === Statistic.MostActiveUsers
+    ) {
+      return this.statisticService.getActiveUsers;
+    }
+
+    if (
+      this.statisticService.getStatisticHeader === Statistic.MostPopularBooks
+    ) {
+      return this.statisticService.getPopularBooks;
+    }
+    return [];
+  }
+}
